1c7ed03fd667a4ba22da2234d63dba23eccc227c
david-med <neo876@hotmail.com>
2023-08-19 08:06:53 -0500

    Update test

--- Sales/Sales.BackEnd/Data/DataContext.cs
+++ Sales/Sales.BackEnd/Data/DataContext.cs
@@ -10,7 +10,7 @@ namespace Sales.BackEnd.Data
 
         }
 
-        public DbSet<Country> Countries { get; set; }
+        public DbSet<Country> Countries { get; set; } 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
